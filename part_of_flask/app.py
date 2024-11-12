484c6e8302ec5dd53f65c230d306ed16af71bd80
akkne <atheros.company@gmail.com>
2024-11-12 17:49:18 +0300

    a

--- part_of_flask/app.py
+++ part_of_flask/app.py
@@ -2,9 +2,9 @@ import flask
 
 app = flask.Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/flask/opo')
+def opo():
     return 'hello from flask'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
