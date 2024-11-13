a72377e9b909100d990542f1da9697fc74650d09
akkne <atheros.company@gmail.com>
2024-11-13 08:17:15 +0300

    Arch Done

--- part_of_django/part_of_django/urls.py
+++ part_of_django/part_of_django/urls.py
@@ -17,9 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from test.views import testt
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('django/test', testt)
 ]
--- part_of_flask/app.py
+++ part_of_flask/app.py
@@ -2,9 +2,5 @@ import flask
 
 app = flask.Flask(__name__)
 
-@app.route('/flask/opo')
-def opo():
-    return 'hello from flask'
-
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
